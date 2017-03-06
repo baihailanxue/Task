05aea67f7952bc571408f6a9e1f627a5a1158ee2
baihailanxue <2463490474@qq.com>
2017-03-06 18:18:54 +0800

    Add files via upload

---
+++ task_01.js
@@ -0,0 +1,9 @@
+﻿window.onload = function () {
+    var button = document.getElementById('button');
+    var input_text = document.getElementById('aqi-input');
+    var display = document.getElementById('aqi-display');
+    //button按钮点击事件
+    button.onclick = function () {
+        display.innerHTML = input_text.value;
+    };
+}
